fae5aa69e6df32b938ffd21e36ed7a2a585c5e44
Vlad805 <zhomyruk_ak18@nuwm.edu.ua>
2020-03-07 18:54:45 +0200

    Limitation of the movement of a figure

--- TetrisBlock.cs
+++ TetrisBlock.cs
@@ -6,6 +6,9 @@ public class TetrisBlock : MonoBehaviour
 {
     private float prevTime;
     public float fallTime = 0.9f;
+    public static int heigh = 20;
+    public static int widht = 12;
+
 
     void Start()
     {
@@ -18,18 +21,38 @@ public class TetrisBlock : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.position += new Vector3(-1, 0, 0);
+            if (!ValidMove())
+                transform.position -= new Vector3(-1, 0, 0);
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             transform.position += new Vector3(1, 0, 0);
+            if (!ValidMove())
+                transform.position -= new Vector3(1, 0, 0);
         }
 
         //vertical movement
         if (Time.time - prevTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime))
         {
             transform.position += new Vector3(0, -1, 0);
+            if (!ValidMove())
+                transform.position -= new Vector3(0, -1, 0);
             prevTime = Time.time;
         }
+    }
+
+    bool ValidMove()
+    {
+        foreach(Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+            if(roundedX < 0 || roundedX >= widht || roundedY < 0 || roundedY >= heigh)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
