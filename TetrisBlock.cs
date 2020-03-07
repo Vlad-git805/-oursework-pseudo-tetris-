53d9cd3c1295409c7321205a609c674f66b8f71c
Vlad805 <zhomyruk_ak18@nuwm.edu.ua>
2020-03-07 18:04:31 +0200

    Move the figure to the right and to the left

--- TetrisBlock.cs
+++ TetrisBlock.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class TetrisBlock : MonoBehaviour {
 
-	// Use this for initialization
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
-	}
+        //Move the figure to the right and to the left
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+    }
 }
